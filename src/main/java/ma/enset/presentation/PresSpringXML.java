98ac1377040cbfc7e70fceaf379aa8273457bf37
HARKAOUI Hajar <hajar15harkaoui@gmail.com>
2025-03-16 11:40:57 +0000

    add Injection via Spring XML

---
+++ src/main/java/ma/enset/presentation/PresSpringXML.java
@@ -0,0 +1,13 @@
+package ma.enset.presentation;
+
+import ma.enset.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXML {
+    public static void main(String[] args) {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+    IMetier metier =  springContext.getBean(IMetier.class);
+        System.out.println("Res = "+metier.calcul());
+    }
+}
